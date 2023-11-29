1d5698619f1ce3374e5ced4cef88255edff825ba
gordonheroa <75142875+gordonheroa@users.noreply.github.com>
2023-11-29 08:09:46 -0500

    Add files via upload

---
+++ Locker_11:29:23_final/background.js
@@ -0,0 +1,84 @@
+let id1;
+let url2;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+function activeforce() {
+    sleep(4000).then(() => {
+        activeforce();
+    });
+}
+function listeners(url1,tabs) {
+chrome.tabs.onActivated.addListener((id1,id2) => {
+    sleep(250).then(() => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let id = tabs[0].id;
+	let url2 = tabs[0].url;
+	if (url2 != url1 && String(url2.slice(0,30)) != "https://docs.google.com/forms/" && String(url2.slice(0,20)) != "chrome://extensions/" && String(url2.slice(0,22)) != "docs.google.com/forms/") {
+            /*
+            chrome.notifications.create('', {
+                title: 'Tab has been closed',
+                message: url1 + ' '+ url2,
+                iconUrl: '/chrome.png',
+                type: 'basic'
+            });
+            */
+            chrome.tabs.remove(id);
+        } else {
+            /*
+            chrome.notifications.create('', {
+                title: 'Tab has not been closed',
+                message: url1 + ' '+ url2,
+                iconUrl: '/chrome.png',
+                type: 'basic'
+            });
+            */
+        }
+    });
+    });
+});
+let active = true;
+chrome.windows.onBoundsChanged.addListener(() => {
+    if (!active) { return; }
+        chrome.windows.update(tabs[0].windowId,{state:"maximized",focused:true});
+	active = false;
+	sleep(250).then(() => {
+	    chrome.windows.update(tabs[0].windowId,{state:"fullscreen",focused:true});
+            chrome.windows.update(tabs[0].windowId,{focused:true});
+	    active = true;
+        });
+    });
+    chrome.windows.onFocusChanged.addListener(() => {
+	if (!active) { return; }
+	active = false;
+	sleep(250).then(() => {
+	    chrome.windows.update(tabs[0].windowId,{focused:true});
+	    active = true;
+        });	    
+    });
+}
+var activate = true;
+chrome.runtime.onMessage.addListener(() => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        url1 = (' ' + tabs[0].url).slice(1);
+        id1 = tabs[0].id;
+        chrome.notifications.create('', {
+            title: url1,
+            message: 'Tab has been locked',
+            iconUrl: '/chrome.png',
+            type: 'basic'
+        });
+        if (activate) { 
+	    chrome.windows.update(tabs[0].windowId,{state:"fullscreen"});
+            chrome.windows.update(tabs[0].windowId,{state:"maximized"});
+            chrome.windows.update(tabs[0].windowId,{state:"fullscreen"});
+        }
+        activate = false;
+        listeners(url1,tabs);
+        activeforce(tabs);
+    }); 
+});
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    activeforce(tabs);
+});
+
